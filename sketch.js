3f12aa0b2aaf87848d35a8a8a528569aca76c998
lakshyachowdhary1234 <73855534+lakshyachowdhary1234@users.noreply.github.com>
2021-03-11 17:03:53 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var dog,sadDog,happyDog;
+
+
+function preload(){
+  sadDog=loadImage("Images/Dog.png");
+  happyDog=loadImage("Images/happy dog.png");
+}
+
+function setup() {
+  createCanvas(1000,400);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  feed=createButton("Feed the dog")
+  feed.position(700,95);
+  feed.mousepressed(feedDog)
+
+ addFeed=createButton("add food")
+  addFeed.position(800,95);
+  addFeed.mousepressed(feedDog)
+}
+
+function draw() {
+  background(46,139,87);
+  drawSprites();
+}
+
+//function to read food Stock
+
+
+//function to update food stock and last fed time
+
+
+//function to add food in stock
